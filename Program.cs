0857a6f24ca6d84d4cb00e36f55e366e894ab78e
Chris <ChrisRekfild@gmail.com>
2022-10-26 00:02:13 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using AnnaTest;
+using System.Security.Cryptography.X509Certificates;
+
+FileStream fileStream = new FileStream("C:\\Prog\\AnnaTest1\\AnnaTest\\TextFileToParse.txt", FileMode.Open);
+using (StreamReader reader = new StreamReader(fileStream))
+{
+    List<Detail> details = new List<Detail>();
+
+    string line;
+    while( (line = reader.ReadLine()) != null)
+    {
+        string[] fields = line.Split('%');
+        //Console.WriteLine(line);
+        
+        Detail detail = new Detail();
+        detail.No = Convert.ToInt32(fields[0]);
+        detail.Name = fields[1];
+        detail.Qty = Convert.ToInt32(fields[2]);
+
+        //var query = from d in details
+        //            where d.Name == detail.Name
+        //            && d.No == detail.Qty
+        //            select d;
+
+        var query = details.Find(x => x.No == detail.No && x.No == detail.No);
+
+        if (query == null)
+        {
+            details.Add(detail);
+        } else
+        {
+            query.Qty += detail.Qty;            
+        }
+    }
+
+    foreach( Detail d in details)
+    {
+        Console.WriteLine($"{d.No}%{d.Name}%{d.Qty}");
+    }
+    
+}
